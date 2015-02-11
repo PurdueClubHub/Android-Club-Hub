3dc5f370e5f942fab0d6ef731efcff883184f2ea
Cameron Heskett <Cameron517@gmail.com>
2015-02-11 18:19:48 -0500

    Added empty Club and User classes

---
+++ app/src/main/java/edu/purdue/purdueclubhub/User.java
@@ -0,0 +1,16 @@
+package edu.purdue.purdueclubhub;
+
+/**
+ * Created by Cameron on 2/11/2015.
+ */
+public class User {
+
+    public String username;
+    private String password;
+    private int userId;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+}

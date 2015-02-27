b4ff36f27b56cf8afdc98a4985827ccdefd17c85
tkimia <tkimia@purdue.edu>
2015-02-26 19:47:44 -0500

    Club View made

--- app/src/main/java/edu/purdue/purdueclubhub/ClubActivity.java
+++ app/src/main/java/edu/purdue/purdueclubhub/ClubActivity.java
@@ -5,19 +5,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-
+import android.widget.TextView;
 
 import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
-import java.util.HashMap;
-import java.util.Map;
 
-
-public class NewClubActivity extends ActionBarActivity {
+public class ClubActivity extends ActionBarActivity {
     //Firebase Reference
     Firebase mFirebaseRef;
 
@@ -26,10 +25,13 @@ public class NewClubActivity extends ActionBarActivity {
     SharedPreferences prefs;
     String UID;
 
+    //calling activity's intent
+    Intent calling;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_club);
+        setContentView(R.layout.activity_club);
 
         Firebase.setAndroidContext(this);
         mFirebaseRef = new Firebase(getResources().getString(R.string.firebase_url));
@@ -39,9 +41,34 @@ public class NewClubActivity extends ActionBarActivity {
 
         AuthData auth = mFirebaseRef.getAuth();
         UID = auth.getUid();
+
+        calling = getIntent();
+        final String club_name = calling.getStringExtra("Club");
+
+        mFirebaseRef.child("clubs").child(club_name).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot != null)
+                {
+                    ((TextView)findViewById(R.id.activity_club_title)).setText(club_name);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_purdue_club_hub, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -57,23 +84,4 @@ public class NewClubActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void onCreateClub(View v) {
-        String club_name = ((EditText)findViewById(R.id.cc_name_input)).getText().toString();
-        String club_desc = ((EditText)findViewById(R.id.cc_club_info)).getText().toString();
-        String[] club_admins = new String[5];
-        club_admins[0] = UID;
-
-        Map<String, Object> club = new HashMap<String, Object>();
-        club.put("description", club_desc);
-        club.put("officers", club_admins);
-
-        mFirebaseRef.child("clubs").child(club_name).setValue(club);
-
-        Intent i = new Intent(this, ClubActivity.class);
-        i.putExtra("Club", club_name);
-        startActivity(i);
-        finish();
-    }
-
 }

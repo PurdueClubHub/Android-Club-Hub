65bb77a909c689ba2213d3c3f3ce79a89ce3290c
Shade390 <dylanlsmith390@gmail.com>
2015-03-23 17:14:29 -0400

    Club View Activity gives preference CLUB_NAME value clubname when posts is pushed.

--- app/src/main/java/edu/purdue/purdueclubhub/ClubViewActivity.java
+++ app/src/main/java/edu/purdue/purdueclubhub/ClubViewActivity.java
@@ -96,6 +96,9 @@ public class ClubViewActivity extends ActionBarActivity {
                 //Intent i = new Intent(getBaseContext(), HomePageActivity.class);
                 //i.putExtra("Club",clubname);
                 //startActivity(i);
+                SharedPreferences prefs = getSharedPreferences(getResources().getString(R.string.prefs_name), MODE_PRIVATE);
+                SharedPreferences.Editor prefsEdit = prefs.edit();
+                prefs.edit().putString("CLUB_NAME", clubname).apply();
                 finish();
             }
         });
